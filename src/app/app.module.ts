80820ffea4246f01b444b1451fea14569c9986a6
arismav <arismav20@gmail.com>
2025-10-14 00:38:59 +0300

    final angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { environment } from 'src/environments/environment';
@@ -75,7 +74,6 @@ const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
         provideAuth(() => getAuth()),
         AngularFireAuthModule,
         AngularFirestoreModule,
-        AngularFireStorageModule,
         AngularFireDatabaseModule,
         BrowserAnimationsModule,
         ReactiveFormsModule,
