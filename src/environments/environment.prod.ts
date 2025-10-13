fbc02c425eb2d67921a66aff7a31d7f765bd9f95
arismav <arismav20@gmail.com>
2025-10-13 18:48:07 +0300

    commit before upgrades

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  api : {
+    mainUrl : 'https://exo-api.onrender.com/api',
+    auth : '/auth/local',
+    register: '/auth/local/register',
+    forgotPass: '/auth/forgot-password'
+  },
+  firebase : {
+    apiKey: "AIzaSyDw-N0r7_qEK4idD3pMLo79t3H-Gl93Xa8",
+    authDomain: "app-mav-oct-22.firebaseapp.com",
+    projectId: "app-mav-oct-22",
+    storageBucket: "app-mav-oct-22.appspot.com",
+    messagingSenderId: "70056849430",
+    appId: "1:70056849430:web:e20bd253857623bb2e4537"
+  }
 };
